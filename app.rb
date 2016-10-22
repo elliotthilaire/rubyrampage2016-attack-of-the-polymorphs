dc3c54ad961dbabc3af6715aafbc9c93d7f32092
Elliott Hilaire <elliott.hilaire@gmail.com>
2016-10-22 12:48:03 +0100

    Story page in session

--- app.rb
+++ app.rb
@@ -3,42 +3,39 @@ require 'sinatra'
 enable :sessions
 
 get '/' do
-  if session[:page]
-    redirect to session[:page]
-  else
-    redirect to '/to-kick-or-not-to-kick'
-  end
+  session[:page] = 'to-kick-or-not-to-kick' unless session[:page]
 
+  erb "story/#{session[:page]}".to_sym
 end
 
 get '/to-kick-or-not-to-kick' do
-  session[:page] = '/to-kick-or-not-to-kick'
+  session[:page] = 'to-kick-or-not-to-kick'
   erb :'story/to-kick-or-not-to-kick'
 end
 
 get '/run-away' do
-  session[:page] = '/run-away'
-  erb :'story/run-away'
+  session[:page] = 'run-away'
+  redirect to '/'
 end
 
 get '/abduction' do
-  session[:page] = '/abduction'
-  erb :'story/abduction'
+  session[:page] = 'abduction'
+  redirect to '/'
 end
 
 get '/first-encounter' do
-  session[:page] = '/first-encounter'
-  erb :'story/first-encounter'
+  session[:page] = 'first-encounter'
+  redirect to '/'
 end
 
 get '/talking-sheep' do
-  session[:page] = '/talking-sheep'
-  erb :'story/talking-sheep'
+  session[:page] = 'talking-sheep'
+  redirect to '/'
 end
 
 get '/fin' do
-  session[:page] = '/fin'
-  erb :'story/fin'
+  session[:page] = 'fin'
+  redirect to '/'
 end
 
 get '/play-again' do
