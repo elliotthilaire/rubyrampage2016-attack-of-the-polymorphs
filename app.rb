68f47cd89a28060c68b7f52b5968768d084d22b5
Elliott Hilaire <elliott.hilaire@gmail.com>
2016-10-23 17:13:41 +0100

    Add reset and handle unknown routes

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 require 'sinatra'
 
 enable :sessions
@@ -12,22 +13,40 @@ get '/play-again' do
   redirect to '/'
 end
 
-get '/:page' do
-  session[:page] = params[:page]
-  increment_deaths_if_dead
-
-  # erb "story/#{session[:page]}".to_sym
+get '/reset' do
+  session.clear
   redirect to '/'
 end
 
-before do
-  increment_deaths_if_dead
+get '/:page' do
+  if File.exist?("views/story/#{params[:page]}.erb")
+    session[:page] = params[:page]
+    redirect to '/'
+  else
+    halt 404
+  end
 end
 
-def increment_deaths_if_dead
-  death_scenes = %w(sheep-death push-button sheep-transformation)
+not_found do
+  increment_deaths
+  erb :'404'
+end
+
+error do
+  increment_deaths
+  erb :'500'
+end
+
+before do
+  increment_deaths if dead?
+end
+
+def increment_deaths
   session[:deaths] = 0 unless session[:deaths]
-  if death_scenes.include? params[:page]
-    session[:deaths] += 1
-  end
+  session[:deaths] += 1
+end
+
+def dead?
+  death_scenes = %w(sheep-death push-button sheep-transformation)
+  death_scenes.include? params[:page]
 end
